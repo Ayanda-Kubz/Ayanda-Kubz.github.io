9565310ca57c2a9b5074d29322a0808058f1ff50
Ayanda <ayandakubeka@thedigitalacademy.co.za>
2019-06-26 14:11:42 +0200

    Template addition

---
+++ data/data.js
@@ -0,0 +1,82 @@
+
+
+// Change your profile data here
+let profile = {
+  picture: "",
+  name: "Luntu Ncwadi",
+  description:
+    "I'm a Junior Full Stack developer with a high aptitude for assimilating new skills and technologies .",
+  bio:
+    "I've been programming for 4 years. have 18+ months of professional work experience after graduating.I'm currenlty working as an entry level full stack developer with  Bsc degree in Computer Science . I find problem solving and computer programming fascinating and willing to do everything to advance myself in this field and other aspects of computing as well.Besides the studies, I am ready for the next challenge",
+  email: "lteencwadi@gmail.com",
+  location: "Johannesburg ,Gauteng",
+  cell:"078 398 5285", // YYYY-MM-DD format
+  skills: [
+    "J2EE",
+    "JSE",
+    "Angular7",
+    "HTML5/CSS",
+    "Bootstrap", 
+    "MySQL",
+    "PHP",
+    "JavaScript",
+    "Jquery",
+    "Ajax",
+    "Spring Boot",
+    "zend Framework",
+    "API",
+    "Net beans",
+    "IntelliJ IDEA" ,
+    "Visual studio code",
+     "Git  and Github",
+    "Trello"
+  ],
+  social: {
+    linkedin:"https://www.linkedin.com/in/luntu-ncwadi-045395144/",
+    stackoverflow: "https://stackoverflow.com/users/8700961/guim-gonzalez",
+  },
+  experience: [
+    {
+      title: "Fullstack web developer",
+      company: "freshmarksystems",
+      url: "http://www.freshmarksystems.co.za/",
+      location: "East London, Eastern Cape",
+      description:
+        "I developed web Applications using PHP,Zendframework,HTML,Bootstrap,Jquery,Javascript and MariaDB", from: "Jan 2018",
+      to: "Jan 2019"
+    },
+    {
+      title: "Fullstack developer",
+      company: "The Digital Academy",
+      url: "https://www.thedigitalacademy.co.za/",
+      location: "Johannesburg, Gauteng",
+      description:
+        "I design develop a hybrid mobile Application. using Angular7, ionic4 ,firebase ,Springboot and Mysql",
+      from: " March 2019",
+      to: "Now"
+    }
+  ],
+  education: [
+    {
+      school: "University of Fort hare",
+      degree: "Bachelor Honours Degree ",
+      fieldOfStudy: "Computer Science",
+      location: "Alice, Eastern Cape",
+      graduated: "May 2018",
+      to: "Now"
+    }
+  ],
+  quote: "I turn coffee into code"
+};
+
+let page = {
+  language: "english",
+ 
+  theme:
+   // "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+   // - "purpledream"
+   - "cutekitty"
+};
