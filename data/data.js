dac5647a386d64486a6c1ac07f21054cf48815c9
Ayanda <ayandaujkubeka@gmail.com>
2019-07-02 15:07:03 +0200

    Commit

--- data/data.js
+++ data/data.js
@@ -7,44 +7,29 @@ let profile = {
   description:
     "I'm a Junior Full Stack developer with a high aptitude for assimilating new skills and technologies .",
   bio:
-    "I've been programming for 4 years. have 18+ months of professional work experience after graduating.I'm currenlty working as an entry level full stack developer with  Bsc degree in Computer Science . I find problem solving and computer programming fascinating and willing to do everything to advance myself in this field and other aspects of computing as well.Besides the studies, I am ready for the next challenge",
-  email: "lteencwadi@gmail.com",
+    "I've been programming for 4 years. have 6+ months of professional work experience after graduating.I'm currenlty working as an entry level full stack developer with  Software Development diploma.I am a software developer with knowledge i have gained since the beginning of my journey in software development, I have  experience in different backend and frontend languages, databases and responsive frameworks. I seek an opportunity in Java, AngularJS, Full Stack Development and Database Management.",
+  email: "ayandaujkubeka@gmail.com",
   location: "Johannesburg ,Gauteng",
   cell:"078 398 5285", // YYYY-MM-DD format
   skills: [
-    "J2EE",
-    "JSE",
+    
     "Angular7",
-    "HTML5/CSS",
-    "Bootstrap", 
+    "HTML5/CSS", 
     "MySQL",
-    "PHP",
     "JavaScript",
-    "Jquery",
-    "Ajax",
     "Spring Boot",
-    "zend Framework",
     "API",
     "Net beans",
     "IntelliJ IDEA" ,
     "Visual studio code",
-     "Git  and Github",
-    "Trello"
+    "Git and Github",
+    "Trello",
+    "Postman"
   ],
   social: {
-    linkedin:"https://www.linkedin.com/in/luntu-ncwadi-045395144/",
-    stackoverflow: "https://stackoverflow.com/users/8700961/guim-gonzalez",
+    linkedin:"https://www.linkedin.com/in/ayanda-kubeka-7a76b0159"
   },
   experience: [
-    {
-      title: "Fullstack web developer",
-      company: "freshmarksystems",
-      url: "http://www.freshmarksystems.co.za/",
-      location: "East London, Eastern Cape",
-      description:
-        "I developed web Applications using PHP,Zendframework,HTML,Bootstrap,Jquery,Javascript and MariaDB", from: "Jan 2018",
-      to: "Jan 2019"
-    },
     {
       title: "Fullstack developer",
       company: "The Digital Academy",
@@ -53,16 +38,16 @@ let profile = {
       description:
         "I design develop a hybrid mobile Application. using Angular7, ionic4 ,firebase ,Springboot and Mysql",
       from: " March 2019",
-      to: "Now"
+      to: "Current"
     }
   ],
   education: [
     {
-      school: "University of Fort hare",
-      degree: "Bachelor Honours Degree ",
-      fieldOfStudy: "Computer Science",
-      location: "Alice, Eastern Cape",
-      graduated: "May 2018",
+      institution: "Rosebank College",
+      diploma: "Software Development",
+      fieldOfStudy: "IT",
+      location: "Braamfontein, Johannesburg",
+      graduated: "June 2018",
       to: "Now"
     }
   ],
@@ -70,7 +55,8 @@ let profile = {
 };
 
 let page = {
-  language: "english",
+  language: "English, Zulu, Sotho",
+
  
   theme:
    // "nightsky"
